0d548a9bed1d0adf5c3a5825000bf177906b0803
unknown <dennis11@live.com>
2022-03-20 23:33:03 -0400

    first commit

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
-
-
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/mern-stack-challenge",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 module.exports = mongoose.connection;
